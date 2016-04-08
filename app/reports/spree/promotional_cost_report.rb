33b9e18fe85e6db545604c1f1b8bf5e9dbc9bf0a
jaspreet21anand <jaspreet21anand@gmail.com>
2016-04-08 18:19:42 +0530

    use union for promotion analysis stats

--- app/reports/spree/promotional_cost_report.rb
+++ app/reports/spree/promotional_cost_report.rb
@@ -1,7 +1,7 @@
 module Spree
   class PromotionalCostReport < Spree::Report
     DEFAULT_SORTABLE_ATTRIBUTE = :promotion_name
-    HEADERS = [:promotion_name, :usage_count, :promotion_discount]
+    HEADERS = [:months_name, :promotion_name, :usage_count, :promotion_discount]
     SEARCH_ATTRIBUTES = { start_date: :promotion_created_from, end_date: :promotion_created_till }
 
     def initialize(options)
@@ -10,21 +10,72 @@ module Spree
     end
 
     def generate(options = {})
-      SpreeReportify::ReportDb[:spree_adjustments___adjustments].
-      join(:spree_promotion_actions___promotion_actions, id: :source_id).
-      join(:spree_promotions___promotions, id: :promotion_id).
-      where(adjustments__source_type: "Spree::PromotionAction").
-      where(promotions__created_at: @start_date..@end_date). #filter by params
-      group(:promotions__id).
-      order(sortable_sequel_expression)
+      initialize_months_table
+      # Spree::Promotion.pluck(:id)
+      _id = 1
+      (adjustments_with_month_name = SpreeReportify::ReportDb[:spree_adjustments___adjustments].
+          join(:spree_promotion_actions___promotion_actions, id: :source_id).
+          join(:spree_promotions___promotions, id: :promotion_id).
+          where(adjustments__source_type: "Spree::PromotionAction", promotions__id: _id).
+          # where(promotions__created_at: @start_date..@end_date). #filter by params
+          select{[
+            Sequel.as(abs(:amount), :promotion_discount),
+            Sequel.as(:promotions__id, :promotions_id),
+            :promotions__name___promotion_name,
+            Sequel.as(MONTHNAME(:adjustments__created_at), :month_name),
+            Sequel.as(YEAR(:adjustments__created_at), :year)
+          ]}
+
+          SpreeReportify::ReportDb[adjustments_with_month_name].
+          right_outer_join(:months, name: :month_name).
+          group(:months_name, :promotions_id).
+          # order(:promotions_id, :sort_year, :number).
+          order(:sort_year, :number).
+          select{[
+            months__number,
+            Sequel.as(IFNULL(year, 2016), :sort_year),
+            Sequel.as(concat(name, ' ', IFNULL(year, 2016)), :months_name),
+            Sequel.as(IFNULL(SUM(promotion_discount), 0), :promotion_discount),
+            Sequel.as(IFNULL(promotion_name, ''), :promotion_name),
+            Sequel.as(count(:promotions_id), :usage_count),
+            Sequel.as(IFNULL(promotions_id, _id), :promotions_id)
+          ]}).union(
+          (adjustments_with_month_name = SpreeReportify::ReportDb[:spree_adjustments___adjustments].
+          join(:spree_promotion_actions___promotion_actions, id: :source_id).
+          join(:spree_promotions___promotions, id: :promotion_id).
+          where(adjustments__source_type: "Spree::PromotionAction", promotions__id: _id).
+          # where(promotions__created_at: @start_date..@end_date). #filter by params
+          select{[
+            Sequel.as(abs(:amount), :promotion_discount),
+            Sequel.as(:promotions__id, :promotions_id),
+            :promotions__name___promotion_name,
+            Sequel.as(MONTHNAME(:adjustments__created_at), :month_name),
+            Sequel.as(YEAR(:adjustments__created_at), :year)
+          ]}
+
+          SpreeReportify::ReportDb[adjustments_with_month_name].
+          right_outer_join(:months, name: :month_name).
+          group(:months_name, :promotions_id).
+          # order(:promotions_id, :sort_year, :number).
+          order(:sort_year, :number).
+          select{[
+            months__number,
+            Sequel.as(IFNULL(year, 2016), :sort_year),
+            Sequel.as(concat(name, ' ', IFNULL(year, 2016)), :months_name),
+            Sequel.as(IFNULL(SUM(promotion_discount), 0), :promotion_discount),
+            Sequel.as(IFNULL(promotion_name, ''), :promotion_name),
+            Sequel.as(count(:promotions_id), :usage_count),
+            Sequel.as(IFNULL(promotions_id, _id), :promotions_id)
+          ]})
+        )
+      end
+    end
+
+    def chart_data
     end
 
     def select_columns(dataset)
-      dataset.select{[
-        Sequel.as(abs(sum(:amount)), :promotion_discount),
-        Sequel.as(count(:promotions__id), :usage_count),
-        :promotions__name___promotion_name
-      ]}
+      dataset
     end
   end
 end
