a750b2d80a8dff34248cb6ed2e15f6c5cbd63117
Benjamin Sion <ben@paneco.com>
2016-12-02 10:36:35 +0800

    Resolve merge conflicts from patch
    error: Patch failed at 0001 Remove dependency on spree_events_tracker
    
    [no-pt-story]

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'spree', github: 'spree/spree', branch: 'master'
-gem 'spree_events_tracker', git: "https://github.com/vinsol/spree_events_tracker.git", branch: 'master'
 
 gemspec
--- app/services/spree/report_generation_service.rb
+++ app/services/spree/report_generation_service.rb
@@ -2,19 +2,26 @@ module Spree
   class ReportGenerationService
 
     REPORTS = {
-      finance_analysis:           [
-                                    :payment_method_transactions, :payment_method_transactions_conversion_rate,
-                                    :sales_performance, :shipping_cost, :sales_tax
-                                  ],
-      product_analysis:           [
-                                    :cart_additions, :cart_removals, :cart_updations,
-                                    :product_views, :product_views_to_cart_additions,
-                                    :product_views_to_purchases, :unique_purchases,
-                                    :best_selling_products, :returned_products
-                                  ],
-      promotion_analysis:         [:promotional_cost, :annual_promotional_cost],
-      trending_search_analysis:   [:trending_search],
-      user_analysis:              [:user_pool, :users_not_converted, :users_who_recently_purchased]
+      finance_analysis: [
+          :sales_performance,
+          :payment_method_transactions,
+          :payment_method_transactions_conversion_rate,
+          :shipping_cost,
+          :sales_tax
+      ],
+      product_analysis: [
+          :best_selling_products,
+          :unique_purchases,
+          :returned_products
+      ],
+      promotion_analysis: [
+          :promotional_cost,
+          :annual_promotional_cost
+      ],
+      user_analysis: [
+          :users_not_converted,
+          :users_who_recently_purchased
+      ]
     }
 
     def self.generate_report(report_name, options)
