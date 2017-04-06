b9eb214aa6c932e76638996d5acb42ccb5bf0ecc
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-04-06 15:59:28 +0530

    overriding default_configuration incase database.yml not found(for e.g., Heroku)

--- lib/spree_admin_insights/engine.rb
+++ lib/spree_admin_insights/engine.rb
@@ -22,17 +22,13 @@ module SpreeAdminInsights
     config.to_prepare &method(:activate).to_proc
 
     config.after_initialize do
-      def connnection_configurations
-        configuration_hash = Rails.configuration.database_configuration[Rails.env].to_h
-        if configuration_hash['adapter'] == 'sqlite3'
-          configuration_hash.merge!({ adapter: 'sqlite' })
-        end
-        configuration_hash
-      end
+      configuration_hash = (ActiveRecord::Base.configurations[Rails.env] ||
+                            Rails.configuration.database_configuration[Rails.env]
+                            ).to_h
+      configuration_hash.merge!({ 'adapter' => 'sqlite' }) if(configuration_hash['adapter'] == 'sqlite3')
 
       # Connect to applications DB using ruby's Sequel wrapper
-      ::SpreeAdminInsights::ReportDb = Sequel.connect(connnection_configurations)
-
+      ::SpreeAdminInsights::ReportDb = Sequel.connect(configuration_hash)
     end
   end
 end
